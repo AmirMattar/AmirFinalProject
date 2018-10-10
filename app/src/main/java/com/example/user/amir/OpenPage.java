84fee30584db434cd3e2f22229f5377ef25847be
AmirMattar <amirmattar40@gmail.com>
2018-10-10 14:07:43 +0300

    hi

---
+++ app/src/main/java/com/example/user/amir/OpenPage.java
@@ -0,0 +1,13 @@
+package com.example.user.amir;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpenPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_page);
+    }
+}
