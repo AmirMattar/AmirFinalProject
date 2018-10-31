48984518d5a11b797d9c3886607a1a3202ee6448
AmirMattar <amirmattar40@gmail.com>
2018-10-31 14:08:51 +0200

    fixed camera and added gallery

--- app/src/main/java/com/example/user/amir/CameraGalleryActivity.java
+++ app/src/main/java/com/example/user/amir/CameraGalleryActivity.java
@@ -3,12 +3,16 @@ package com.example.user.amir;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
 
 public class CameraGalleryActivity extends AppCompatActivity implements View.OnClickListener {
     Button gallery, camera;
@@ -17,6 +21,7 @@ public class CameraGalleryActivity extends AppCompatActivity implements View.OnC
     Bitmap bitmap;
 
     public static final int CAMERA_REQUEST = 0;
+    public static final int SELECT_IMAGE = 1;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,17 +44,30 @@ public class CameraGalleryActivity extends AppCompatActivity implements View.OnC
             Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             startActivityForResult(i, CAMERA_REQUEST);
         } else {
-            //later
+            Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(i, SELECT_IMAGE);
         }
 
     }
+
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             cameraimage.setImageBitmap(photo);
+        } else {
+            if (requestCode == SELECT_IMAGE && resultCode == Activity.RESULT_OK) {
+                Uri targetUri = data.getData();
+                try {
+                    bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                    cameraimage.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
+
     }
 }
-
