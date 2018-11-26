1143425e877b6d6953fdfc0317472922eb8e281b
AmirMattar <amirmattar40@gmail.com>
2018-11-26 13:46:56 +0200

    save image

--- app/src/main/java/com/example/user/amir/CameraGalleryActivity.java
+++ app/src/main/java/com/example/user/amir/CameraGalleryActivity.java
@@ -2,17 +2,24 @@ package com.example.user.amir;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CameraGalleryActivity extends AppCompatActivity implements View.OnClickListener {
     Button gallery, camera;
@@ -55,7 +62,16 @@ public class CameraGalleryActivity extends AppCompatActivity implements View.OnC
 
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
+
             cameraimage.setImageBitmap(photo);
+            String filePath = saveImage(bitmap);
+
+            SharedPreferences prof = getSharedPreferences("myprof",MODE_PRIVATE);
+            SharedPreferences.Editor editor= prof.edit();
+            editor.putString("image",filePath);
+            editor.commit();
+
+
         } else {
             if (requestCode == SELECT_IMAGE && resultCode == Activity.RESULT_OK) {
                 Uri targetUri = data.getData();
@@ -70,4 +86,30 @@ public class CameraGalleryActivity extends AppCompatActivity implements View.OnC
 
 
     }
+
+    public String saveImage(Bitmap bitmap) {
+        File root = Environment.getExternalStorageDirectory();
+
+        String timeStamp = new SimpleDateFormat("yyyMMdd_HHmmss").format(new Date());
+
+        String filePath=root.getAbsolutePath()+"/DCIM/Camera/IMG_"+timeStamp+".jpg";
+        File file = new File(filePath);
+
+        try
+        {
+            file.createNewFile();
+            FileOutputStream ostream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,ostream);
+            ostream.close();
+        }
+        catch (Exception e)
+        {
+           e.printStackTrace();
+            Toast.makeText(this,"Failed to save image",Toast.LENGTH_SHORT).show();
+        }
+        return filePath;
+    }
 }
+
+
+
